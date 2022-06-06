63ca5b38d5e58a7eb101cd66224d2fe70889064e
ashiskumarparida <104816831+ashiskumarparida@users.noreply.github.com>
2022-06-06 20:36:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+resource "aws_config_aggregate_authorization" "ak" {
+  account_id = "860595709477"
+  region     = "ap-south-1"
+}
+provider "aws" {
+  region = "ap-south-1"
+}
 resource "aws_instance" "ec2" {
   ami           = "ami-079b5e5b3971bd10d "
   instance_type = "t2.micro"
