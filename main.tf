3ff3fc571a1a2094263e34128fd59b59472dae52
ASHIS KUMAR PARIDA <kumar.ashis.1997@gmail.com>
2022-06-06 21:35:01 +0530

    adding

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_config_aggregate_authorization" "ak" {
+  account_id = "860595709477"
+  region     = "ap-south-1"
+}
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "ec2" {
+  ami           = "ami-079b5e5b3971bd10d "
+  instance_type = "t2.micro"
+  subnet_id     = "subnet id -09057160738ffdd97"
+  tags = {
+    Name = "Hello"
+  }
+}
