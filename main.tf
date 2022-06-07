feb1a378bff9e69cdf67258d2dca3b6a8d99b383
ASHIS KUMAR PARIDA <kumar.ashis.1997@gmail.com>
2022-06-07 07:34:23 +0530

    modify

--- main.tf
+++ main.tf
@@ -2,8 +2,18 @@ resource "aws_config_aggregate_authorization" "ak" {
   account_id = "860595709477"
   region     = "ap-south-1"
 }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+# Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region     = "ap-south-1"
+  access_key = "AKIA4QX4AMYSRMUDQCHC"
+  secret_key = "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"
 }
 resource "aws_instance" "ec2" {
   ami           = "ami-079b5e5b3971bd10d "
