def1d2a7235710aca29347232d20e224027333d4
ASHIS KUMAR PARIDA <104816831+ashiskumarparida@users.noreply.github.com>
2022-06-07 23:44:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,36 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-
-  cloud {
+  required_version = "~> 0.12"
+  backend "remote" {
+    hostname     = "app.terraform.io"
     organization = "AK_Automated"
-
-    workspaces {
-      name = "Auto-deploy"
-    }
+    workspaces { prefix = "vpc-" }
   }
 }
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+module "vpc" {
+  source = "github.com/nitheesh86/terraform-modules/modules/vpc"
+
+  name = var.name
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = var.env
+  }
+}
+
 
 
 
