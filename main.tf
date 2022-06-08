4cf3b2715325b28f26d7690208852b65511975f0
ASHIS KUMAR PARIDA <104816831+ashiskumarparida@users.noreply.github.com>
2022-06-08 06:50:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,23 @@
-
 terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      Version = "~>3.27"
     }
   }
-  
-  backend "remote" {
-    organization = "app.terraform.io"
 
-    workspaces {
-      name = "sahoo"
-    }
-  }
+  required_version = ">=0.14.9"
+  backend "s3" {
+       bucket = "[Remote_State_S3_Bucket_Name]"
+       key    = "[Remote_State_S3_Bucket_Key]"
+       region = "east-us-1"
+   }
 }
 
+}
 
 provider "aws" {
+  version = "~>3.0"
   region = "ap-south-1"
 }
 
@@ -29,6 +30,31 @@ resource "aws_instance" "ec2" {
   }
 }
 
+resource "aws_s3_bucket" "s3Bucket" {
+     bucket = "[BUCKET_NAME_HERE]"
+     acl       = "public-read"
+
+     policy  = <<EOF
+{
+     "id" : "MakePublic",
+   "version" : "2012-10-17",
+   "statement" : [
+      {
+         "action" : [
+             "s3:GetObject"
+          ],
+         "effect" : "Allow",
+         "resource" : "arn:aws:s3:::[BUCKET_NAME_HERE]/*",
+         "principal" : "*"
+      }
+    ]
+  }
+EOF
+
+   website {
+       index_document = "index.html"
+   }
+}
 
 
 
